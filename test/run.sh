53c82bf28bd9db81bb93bb6088af9c624132baa0
Vincent <xiashuangxi@hotmail.com>
2020-12-10 00:00:03 +0800

    init

--- test/run.sh
+++ test/run.sh
@@ -4,18 +4,5 @@ cd $(dirname $0)
 echo '进入 springboot-main-slave'
 cd ../springboot-main-slave
 chmod 777 ./gradlew
-echo '执行 gradlew clean'
-# ./gradlew clean
-# ret=$?
-# if [ $ret -ne 0 ]; then
-# exit $ret
-# fi
-
-echo '执行 gradlew compileJava'
-./gradlew check
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
+./gradlew build --scan -s
 exit
