6225bb8fb0329e35558e1b5160521cd5bf9d717b
Vincent <xiashuangxi@hotmail.com>
2020-12-11 17:08:17 +0800

    🛠添加mockito依赖

--- springboot-tdd/src/test/java/com/sbe/tdd/service/DemoServiceTest.java
+++ springboot-tdd/src/test/java/com/sbe/tdd/service/DemoServiceTest.java
@@ -1,29 +1,40 @@
 package com.sbe.tdd.service;
 
 import com.sbe.tdd.dto.DemoDTO;
+import com.sbe.tdd.mapper.DemoMapper;
+import com.sbe.tdd.service.impl.DemoServiceImpl;
 import org.apache.ibatis.reflection.ArrayUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
 public class DemoServiceTest {
     @Mock
-    private DemoService demoService;
+    private DemoMapper demoMapper;
+
+    @InjectMocks
+    private DemoService demoService = new DemoServiceImpl();
+
+    @Autowired
+    private DemoServiceImpl demoServiceImpl;
 
     @Test
     public void getDemoDetails_returnsDemoInfo() {
         DemoDTO dto = new DemoDTO();
         dto.setId(1L);
         dto.setContent("xxx");
-        List<DemoDTO> ldtolist = new ArrayList<DemoDTO>();
-        ldtolist.add(dto);
-        BDDMockito.given( demoService.findAll()).willReturn( ldtolist );
+
+        List<DemoDTO> demos = Arrays.asList(dto);
+        BDDMockito.when(demoService.findAll()).thenReturn(demos);
         List<DemoDTO> dtoList = demoService.findAll();
 
         Assertions.assertFalse(dtoList.isEmpty());
@@ -33,7 +44,14 @@ public class DemoServiceTest {
 
     @Test
     public void getDemoDetails_returnsDemoAll() {
+        DemoDTO dto = new DemoDTO();
+        dto.setId(1L);
+        dto.setContent("xxx");
+
+        List<DemoDTO> demos = Arrays.asList(dto);
+
+        BDDMockito.when(demoService.findAll()).thenReturn(demos);
         List<DemoDTO> dtoList = demoService.findAll();
-//        Assertions.assertFalse(dtoList.isEmpty());
+        Assertions.assertFalse(dtoList.isEmpty());
     }
 }
